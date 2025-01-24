a3e8aa3ad6fbc511c15d6a438fcba21e81970680
Rise0x00 <rise.dhmn@gmail.com>
2025-01-24 22:45:43 +0000

    Added SOCKS5 proxy parsing Minor bug fixes

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from bs4 import BeautifulSoup
 
 FILE_NAME = "Proxies.txt"
 
+def get_country_name(iso_code):
+    response = requests.get(f"https://restcountries.com/v3.1/alpha/{iso_code}")
+    if response.status_code == 200:
+        country_data = response.json()
+        return country_data[0]['name']['common']
+    else:
+        return False
 def fetch_https_proxies():
     url = "https://free-proxy-list.net/"
     proxies = []
@@ -63,18 +70,49 @@ def fetch_socks4_proxies():
         version = cols[4].text.strip()
         proxies.append((ip, port, country, version))
     # format proxy_list(ip, port, country, version)    
+    #print(proxies)
     return proxies
 
-def main():
-    protocol = input("Select a protocol ([1] HTTPS, [2] SOCKS4): ").strip().upper()
+def fetch_socks5_proxies():
+    url = "https://freeproxyupdate.com/socks5-proxy/"
+    proxies = []
 
-    if protocol not in ["HTTPS", "SOCKS4", "1", "2"]:
-        print("Error: invalid protocol. Acceptable values: HTTPS, SOCKS4")
+    try:
+        r = requests.get(url, timeout=15)
+        r.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Request error to {url}: {e}")
+        return False
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    table = soup.find("table", class_="list-proxy")
+    if not table:
+        print(f"The proxy table was not found on [ {url} ]")
+        return False
+
+    rows = table.tbody.find_all("tr")
+    for row in rows:
+        cols = row.find_all("td")
+        if len(cols) < 8:
+            continue
+        ip = cols[0].text.strip()
+        port = cols[1].text.strip()
+        country = cols[2].text.strip()
+        speed = cols[5].text.strip()
+        proxies.append((ip, port, country, speed))
+    # format proxy_list(ip, port, country, speed)  
+    print(proxies)  
+    return proxies
+def main():
+    protocol = input("Select a protocol ([1] HTTPS, [2] SOCKS4), [3] SOCKS5: ").strip().upper()
+
+    if protocol not in ["HTTPS", "SOCKS4", "SOCKS5", "1", "2", "3"]:
+        print("Error: invalid protocol. Acceptable values: HTTPS, SOCKS4, SOCKS5")
         main()
 
     country = input("Select a country (two-letter ISO code, for example 'US', 'RU', 'IT') or 'ALL' for all: ").strip().upper()
 
-    if protocol == "HTTPS" or "1":
+    if protocol == "HTTPS" or protocol == "1":
         print("\nFetching HTTPS proxies...")
         proxy_list = fetch_https_proxies()
         result = []
@@ -82,13 +120,26 @@ def main():
             if country == "ALL" or ctry.upper() == country:
                 result.append(f"{ip}:{port}")
 
-    elif protocol == "SOCKS4" or "2":
+    elif protocol == "SOCKS4" or protocol == "2":
         print("\nFetching SOCKS4 proxies...")
         proxy_list = fetch_socks4_proxies()
         result = []
         for ip, port, ctry, version in proxy_list:
-            if version.lower() == protocol.lower():
+            if version.lower() == "SOCKS4".lower():
                 if country == "ALL" or ctry.upper() == country:
+                    result.append(f"{ip}:{port}")
+
+    elif protocol == "SOCKS5" or protocol == "3":
+        print("\nFetching SOCKS5 proxies...")
+        country_name = get_country_name(country)
+        if not country_name:
+            print(f"Error: invalid ISO code '{country}'.")
+            return
+        proxy_list = fetch_socks5_proxies()
+        result = []
+        for ip, port, ctry, speed in proxy_list:
+            if speed.lower() == "fast":
+                if country == "ALL" or ctry.lower() == country_name.lower():
                     result.append(f"{ip}:{port}")
 
     print(f"\nFound proxies: {len(result)}\n")
